c6dfc65ac8bf779e6ad13ad1c49660fb34e9495b
esscbee <github@esscb.com>
2016-07-07 10:12:44 -0700

    initial

---
+++ SpiderWeb/GameScene.swift
@@ -0,0 +1,180 @@
+//
+//  GameScene.swift
+//  SpiderWeb
+//
+//  Created by Stephen Brennan on 7/7/16.
+//  Copyright (c) 2016 Stephen Brennan. All rights reserved.
+//
+
+import SpriteKit
+
+class ScbLine : SKShapeNode {
+    enum States {
+        case Create
+        case Drag
+    }
+    var state = States.Create
+    
+    let start : CGPoint
+    var end : CGPoint?
+    var dragStart : CGPoint?
+    
+    init(start : CGPoint, scene: SKScene) {
+        self.start = start
+        super.init()
+        let path = CGPathCreateMutable()
+        CGPathMoveToPoint(path, nil, start.x, start.y)
+        self.path = path
+        self.lineWidth = 5
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func touchBegin(location : CGPoint) {
+        dragStart = location
+        state = .Drag
+    }
+    
+    func move(point : CGPoint) {
+        if(state == .Create) {
+            let path = CGPathCreateMutable()
+            CGPathMoveToPoint(path, nil, start.x, start.y)
+            CGPathAddLineToPoint(path, nil, point.x, point.y)
+            self.path = path
+            end = point
+        } else {
+            let dx = point.x - dragStart!.x
+            let dy = point.y - dragStart!.y
+            var xlat = CGAffineTransformMakeTranslation(dx, dy)
+            let mp = CGPathCreateCopyByTransformingPath(self.path, &xlat)
+            self.path = mp
+            dragStart = point
+        }
+    }
+    
+    func checkNear(p1 : CGPoint, p2: CGPoint) -> Bool {
+        let dx = pow(p1.x - p2.x, 2)
+        let dy = pow(p1.y - p2.y, 2)
+        let mag = pow(dx + dy, 0.5)
+        return mag < 20
+    }
+    
+    func near(point : CGPoint) -> CGPoint? {
+        var toProcess = [ start ]
+        if let e = end {
+            toProcess.append(e)
+        }
+        for p in toProcess {
+            if checkNear(p, p2: point) {
+                return p
+            }
+        }
+        return nil
+    }
+    func intersectsPoint(p : CGPoint) -> Bool {
+        var points = [CGPoint]()
+        
+        withUnsafeMutablePointer(&points) { pPoints in
+            CGPathApply(path, pPoints, {
+                ptr, ele in
+                let pe : CGPathElement = ele.memory
+                if let pt = pe.points.memory as? CGPoint {
+                    let arr = UnsafeMutablePointer<[CGPoint]>(ptr)
+                    arr.memory.append(pt)
+                }
+            })
+        }
+        let dx1 = points[0].x - points[1].x
+        let dy1 = points[0].y - points[1].y
+        let slope1 = dx1 / dy1
+        
+        let dx2 = points[0].x - p.x
+        let dy2 = points[0].y - p.y
+        let slope2 = dx2 / dy2
+        let ds = abs(slope2 - slope1)
+        
+        print("slope1 \(slope1), slope2 \(slope2), ds: \(ds)")
+        
+        return ds < 0.1;
+    }
+    func computeNear() -> Bool {
+        return state == .Create
+    }
+}
+
+class GameScene: SKScene {
+    var touchMap = [ UITouch : ScbLine ]()
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for t in touches {
+            var location = t.locationInNode(self)
+            var found : ScbLine?
+            for hit in self.nodesAtPoint(location) {
+                if let n = hit as? ScbLine {
+                    if n.intersectsPoint(location) {
+                        found = n
+                        break
+                    }
+                }
+            }
+            if let n = found {
+                touchMap[t] = n
+                n.touchBegin(location)
+            } else {
+                if let p = near(location) {
+                    location = p
+                }
+                let tad = ScbLine(start: location, scene: self)
+                touchMap[t] = tad
+                self.addChild(tad)
+            }
+            
+        }
+    }
+    
+    func near(inPoint : CGPoint) -> CGPoint? {
+        for c in self.children {
+            if let l = c as? ScbLine {
+                if let p = l.near(inPoint) {
+                    return p
+                }
+            }
+        }
+        return nil
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for t in touches {
+            let location = t.locationInNode(self)
+            if let tad = touchMap[t] {
+                tad.move(location)
+            }
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for t in touches {
+            var location = t.locationInNode(self)
+            if let tad = touchMap[t] {
+                touchMap.removeValueForKey(t)
+                if tad.computeNear() {
+                    
+                    if let p = near(location) {
+                        location = p
+                    }
+                }
+                tad.move(location)
+                
+            }
+        }
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
