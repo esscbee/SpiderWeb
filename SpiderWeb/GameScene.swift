cebacc9a3c59c96fba690ffbd266ecbf910614b3
esscbee <github@esscb.com>
2016-07-07 11:44:13 -0700

    drag different parts of the line

--- SpiderWeb/GameScene.swift
+++ SpiderWeb/GameScene.swift
@@ -8,10 +8,19 @@
 
 import SpriteKit
 
+func computeMagnitude(p1:CGPoint, p2: CGPoint) -> CGFloat{
+    let dx = pow(p1.x - p2.x, 2)
+    let dy = pow(p1.y - p2.y, 2)
+    
+    return pow(dx + dy, 0.5)
+}
+
 class ScbLine : SKShapeNode {
     enum States {
         case Create
-        case Drag
+        case DragStart
+        case DragEnd
+        case DragMiddle
     }
     var state = States.Create
     
@@ -32,32 +41,78 @@ class ScbLine : SKShapeNode {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func computeMidPoint() -> CGPoint {
+        let pts = getPoints()
+        let p1 = pts.p1
+        let p2 = pts.p2
+        
+        let dx2 = abs(p1.x - p2.x) / 2.0
+        let dy2 = abs(p1.y - p2.y) / 2.0
+        let mx = min([p1.x, p2.x]) + dx2
+        let my = min([p1.y, p2.y]) + dy2
+        
+        return CGPoint(x: mx, y: my)
+        
+    
+    }
+    func min(arr : [CGFloat]) -> CGFloat {
+        var ret = CGFloat.max
+        
+        for f in arr {
+            if f < ret {
+                ret = f
+            }
+        }
+        return ret
+    }
     func touchBegin(location : CGPoint) {
-        dragStart = location
-        state = .Drag
+        let mp = computeMidPoint()
+        let pts = getPoints()
+        let p1m = computeMagnitude(location, p2: pts.p1)
+        let p2m = computeMagnitude(location, p2: pts.p2)
+        let mpm = computeMagnitude(location, p2: mp)
+        
+        let m = min([p1m, p2m, mpm])
+        
+        if m == p1m {
+            state = .DragStart
+        } else if m == p2m {
+            state = .DragEnd
+        } else {
+            state = .DragMiddle
+            dragStart = location
+        }
+        start
     }
     
     func move(point : CGPoint) {
-        if(state == .Create) {
+        switch (state) {
+        case .Create:
             let path = CGPathCreateMutable()
             CGPathMoveToPoint(path, nil, start.x, start.y)
             CGPathAddLineToPoint(path, nil, point.x, point.y)
             self.path = path
             end = point
-        } else {
+        case .DragMiddle:
             let dx = point.x - dragStart!.x
             let dy = point.y - dragStart!.y
             var xlat = CGAffineTransformMakeTranslation(dx, dy)
             let mp = CGPathCreateCopyByTransformingPath(self.path, &xlat)
             self.path = mp
             dragStart = point
+        case .DragStart, .DragEnd:
+            let pts = getPoints()
+            let sp = state == .DragStart ? point : pts.p1
+            let ep = state == .DragEnd ? point : pts.p2
+            let path = CGPathCreateMutable()
+            CGPathMoveToPoint(path, nil, sp.x, sp.y)
+            CGPathAddLineToPoint(path, nil, ep.x, ep.y)
+            self.path = path
         }
     }
     
     func checkNear(p1 : CGPoint, p2: CGPoint) -> Bool {
-        let dx = pow(p1.x - p2.x, 2)
-        let dy = pow(p1.y - p2.y, 2)
-        let mag = pow(dx + dy, 0.5)
+        let mag = computeMagnitude(p1, p2: p2)
         return mag < 20
     }
     
@@ -73,7 +128,7 @@ class ScbLine : SKShapeNode {
         }
         return nil
     }
-    func intersectsPoint(p : CGPoint) -> Bool {
+    func getPoints() -> (p1: CGPoint, p2: CGPoint) {
         var points = [CGPoint]()
         
         withUnsafeMutablePointer(&points) { pPoints in
@@ -86,21 +141,30 @@ class ScbLine : SKShapeNode {
                 }
             })
         }
-        let dx1 = points[0].x - points[1].x
-        let dy1 = points[0].y - points[1].y
+        
+        return (points[0], points[1])
+        
+    }
+    func intersectsPoint(p : CGPoint) -> Bool {
+        let ps = getPoints()
+        let p1 = ps.p1
+        let p2 = ps.p2
+        
+        let dx1 = p1.x - p2.x
+        let dy1 = p1.y - p2.y
         let slope1 = dx1 / dy1
         
-        let dx2 = points[0].x - p.x
-        let dy2 = points[0].y - p.y
+        let dx2 = p1.x - p.x
+        let dy2 = p1.y - p.y
         let slope2 = dx2 / dy2
         let ds = abs(slope2 - slope1)
         
-        print("slope1 \(slope1), slope2 \(slope2), ds: \(ds)")
+        // print("slope1 \(slope1), slope2 \(slope2), ds: \(ds)")
         
         return ds < 0.1;
     }
     func computeNear() -> Bool {
-        return state == .Create
+        return state != .DragMiddle
     }
 }
 
